5a4e8480b195d9da1e2aa96144dfcab49b3ef0e8
Akram Khan <akramkhan83@gmail.com>
2012-07-05 11:14:44 +0530

    first commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,20 @@
+
+class HomeController < ApplicationController
+	def index
+	end
+
+	def about
+	end
+
+	def support
+	end
+
+	def terms_and_condition
+	end
+
+	def privacy
+	end
+
+	def learn_more
+	end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Monoworld::Application.config.secret_token = 'ebc925dd6b67a3414959568f69d1a02cd2176d80cc0c5c3638353515dbec722efe4b74ba137ba024a08c1cf34673800802620790b2e4a887887a012047cbec8e'
