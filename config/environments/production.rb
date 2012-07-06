b3a7bffcb3527d1c9dcfd9fd3d172adc5d1b6498
Akram Khan <akramkhan83@gmail.com>
2012-07-06 20:44:13 +0530

    added smtp in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,18 @@ Monoworld::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    :enable_starttls_auto => true,
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :domain => "gmail.com",
+    :authentication => :login,
+    :user_name => "akram@akaruilabs.com",
+    :password => "workatror83",
+  }
+
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
