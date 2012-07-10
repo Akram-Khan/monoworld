aca32fc2717db021fd8fe7c2157f3cd0d2918fcc
Akram Khan <akramkhan83@gmail.com>
2012-07-10 16:53:47 +0530

    created like model and controller

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,3 @@
+class LikesController < ApplicationController
+	before_filter :authenticate_user!
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120710044442) do
+ActiveRecord::Schema.define(:version => 20120710105844) do
 
   create_table "comments", :force => true do |t|
     t.text     "comment"
@@ -23,6 +23,16 @@ ActiveRecord::Schema.define(:version => 20120710044442) do
 
   add_index "comments", ["photo_id"], :name => "index_comments_on_photo_id"
   add_index "comments", ["user_id"], :name => "index_comments_on_user_id"
+
+  create_table "likes", :force => true do |t|
+    t.integer  "photo_id"
+    t.integer  "user_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "likes", ["photo_id"], :name => "index_likes_on_photo_id"
+  add_index "likes", ["user_id"], :name => "index_likes_on_user_id"
 
   create_table "photos", :force => true do |t|
     t.string   "name"
